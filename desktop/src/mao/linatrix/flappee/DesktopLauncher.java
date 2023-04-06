8413a528953b34fbc46fd125b74ce915da4444d6
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-04-06 17:19:51 +0200

    Starting flappee-project by an empty set

---
+++ desktop/src/mao/linatrix/flappee/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package mao.linatrix.flappee;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import mao.linatrix.flappee.Flappee;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Flappee");
+		new Lwjgl3Application(new Flappee(), config);
+	}
+}
